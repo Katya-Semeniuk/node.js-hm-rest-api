a250568c1c4982db4f7c0423c1c73d228e922511
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-04-25 12:47:48 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require("./app");
+// ukcz2D9OyMrbMfkK
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Katya:ukcz2D9OyMrbMfkK@cluster0.dqzme7v.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
